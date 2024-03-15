c5d97deab2a8d0c9f27031424d1aac923449b95e
matheusmota <mthsfm73@gmail.com>
2024-03-14 22:55:48 -0300

    df

--- dataframes.py
+++ dataframes.py
@@ -1,5 +1,19 @@
 import pandas as pd
 
-df = pd.read_csv('customers.csv')
 
-print(df)
+dados = [10, 20, 42, 9, 12, 35, 24, 10, 8, 14, 21]
+print(dados)
+series = pd.Series(dados)
+#print(series)
+
+# %%
+media = series.mean()
+print(media)
+
+# %%
+desvio = series.std()
+print(desvio)
+
+# %%
+maximo = series.max()
+print(maximo)
